67d881c56dd766ca1ac7ee32a9b057727f903972
jiabin1122 <shuixian8812@163.com>
2017-12-05 23:16:39 +0800

    ignore account info

--- runtest.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'jiabin5'
-# coding: utf-8
-
-import pytest
-
-exit(pytest.main("-sq ./case/test_nav"))
--- utils/browserCli.py
+++ utils/browserCli.py
@@ -1,3 +1,5 @@
+import yaml
+
 __author__ = 'jiabin5'
 # coding: utf-8
 
@@ -6,11 +8,14 @@ from selenium import webdriver
 from utils.set_cookies import save_cookies
 import time
 
-with open("./data/account.txt", "r") as f :
-    for item in f.readlines():
-        line = item.split(":")
-        if line[0] == "user1":
-            user, pwd = line[1].split(",")[0], line[1].split(",")[1].strip()
+
+with open("./data/account.yaml", "r") as f :
+    inf = yaml.load(f)
+    for key, value in inf.items():
+        if key == "account" :
+            user = value
+    for key, value in user.items():
+        userName, pwd = key, value
 
 class browserClient(object):
     def __init__(self):
@@ -25,7 +30,7 @@ class browserClient(object):
         driver.switch_to.frame("login_frame")
         driver.implicitly_wait(3)
         driver.find_element_by_id("loginname").clear()
-        driver.find_element_by_id("loginname").send_keys(user)
+        driver.find_element_by_id("loginname").send_keys(userName)
         driver.find_element_by_id("nloginpwd").clear()
         driver.find_element_by_id("nloginpwd").send_keys(pwd)
         time.sleep(10)
