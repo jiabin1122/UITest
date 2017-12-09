2d9b2b5e0c8f779b28beb2593ba7c8c42971e715
jiabin1122 <shuixian8812@163.com>
2017-12-09 19:22:38 +0800

    add dw case...

--- case/test_dw/test_dw.py
+++ case/test_dw/test_dw.py
@@ -8,12 +8,16 @@ import time
 from utils.randomUtils import generate_random_string
 
 driver = br.driver
-sql = "select * from a_atest.jia_6cols"
+sql = "select * from a_atest.jia_6cols;"
 task_name = "jia_uitest_" + generate_random_string(5)
 task_name_rename = "jia_uitest_" + generate_random_string(5)
-task_name_save_as = task_name_rename + "_save_as"
+task_name_save_as = "jia_uitest_" + generate_random_string(5)
 
 class Test_dw():
+    def task_search(self, name):
+        br.send_keys(name, By.XPATH, "//input[@class='s-searchInput']")
+        br.click(By.XPATH, "//i[@class='i31']")
+
     def test_create_query_task(self):
         """
         新建sql查询脚本
@@ -32,8 +36,7 @@ class Test_dw():
         br.send_keys(task_name, By.XPATH, "//input[@class='inputMode']")
         br.click(By.XPATH, "//a[contains(text(),'确定')]")
         br.mouse_click(By.XPATH, "//p[contains(text(),'开发脚本')]")
-        br.send_keys(task_name, By.XPATH, "//input[@class='s-searchInput']")
-        br.click(By.XPATH, "//i[@class='i31']")
+        self.task_search(task_name)
         assert is_element_exist(driver, "//h3[contains(text(),'{0}')]".format(task_name))
 
     def test_task_rename(self):
@@ -42,9 +45,34 @@ class Test_dw():
         br.mouse_click(By.XPATH, "//a[contains(text(),'重命名')]")
         br.send_keys(task_name_rename, By.XPATH, "//input[@class='inputMode']")
         br.click(By.XPATH, "//a[contains(text(),'确定')]")
-        br.send_keys(task_name_rename, By.XPATH, "//input[@class='s-searchInput']")
-        br.click(By.XPATH, "//i[@class='i31']")
+        self.task_search(task_name_rename)
         assert is_element_exist(driver, "//h3[contains(text(),'{0}')]".format(task_name_rename))
-        br.send_keys(task_name, By.XPATH, "//input[@class='s-searchInput']")
-        br.click(By.XPATH, "//i[@class='i31']")
+        self.task_search(task_name)
         assert False == is_element_exist(driver, "//h3[contains(text(),'{0}')]".format(task_name))
+
+    def test_task_save_as(self):
+        br.mouse_send_keys("show databases;", By.XPATH, "//div[@class='ace_content']")
+        br.click(By.XPATH, "//a[contains(text(),'另存为')]")
+        br.send_keys(task_name_save_as, By.XPATH, "//input[@class='inputMode']")
+        br.click(By.XPATH, "//a[contains(text(),'确定')]")
+        time.sleep(2)
+        br.click(By.XPATH, "//a[contains(text(),'运行')]")
+        time.sleep(5)
+        assert is_element_exist(driver,"//tr/th[contains(text(),'databaseName')]")
+
+    def test_delete_task(self):
+        self.task_search(task_name_rename)
+        br.mouse_move(By.XPATH, "//h3[contains(text(),'{0}')]".format(task_name_rename))
+        br.mouse_move(By.XPATH, "//dt/span[contains(text(),'...')]")
+        br.mouse_click(By.XPATH, "//a[contains(text(),'删除')]")
+        br.click(By.XPATH, "//a[contains(text(),'确定')]")
+        self.task_search(task_name_rename)
+        assert False == is_element_exist(driver, "//h3[contains(text(),'{0}')]".format(task_name_rename))
+        self.task_search(task_name_save_as)
+        br.mouse_move(By.XPATH, "//h3[contains(text(),'{0}')]".format(task_name_save_as))
+        br.mouse_move(By.XPATH, "//dt/span[contains(text(),'...')]")
+        br.mouse_click(By.XPATH, "//a[contains(text(),'删除')]")
+        br.click(By.XPATH, "//a[contains(text(),'确定')]")
+
+
+
